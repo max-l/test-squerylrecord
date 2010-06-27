008f743b25bc799ae231c7fb8b4d991a972f4bb5
Ross Mellgren <rmm-github@z.odi.ac>
2010-06-27 01:00:06 -0400

    bump versions up

--- project/build/LiftProject.scala
+++ project/build/LiftProject.scala
@@ -1,9 +1,9 @@
 import sbt._
 
 class LiftProject(info: ProjectInfo) extends DefaultWebProject(info) {
-    val databinder = "Databinder Repository" at "http://databinder.net/repo"
     val ivyLocal = "Local Ivy Repository" at "file://"+Path.userHome+"/.ivy2/local"
     val mavenLocal = "Local Maven Repository" at "file://"+Path.userHome+"/.m2/repository"
+    val databinder = "Databinder Repository" at "http://databinder.net/repo"
     //val scalatools_snapshot = "Scala Tools Snapshot" at "http://scala-tools.org/repo-snapshots/"
     //val scalatools_release = "Scala Tools Snapshot" at "http://scala-tools.org/repo-releases/"
 
@@ -13,7 +13,7 @@ class LiftProject(info: ProjectInfo) extends DefaultWebProject(info) {
         "net.liftweb" % "lift-webkit" % liftVersion % "compile->default",
         "net.liftweb" % "lift-record" % liftVersion % "compile->default",
         "net.liftweb" % "lift-squeryl-record" % liftVersion % "compile->default",
-        "org.squeryl" % "squeryl_2.8.0.Beta1" % "0.9.4beta2" % "compile->runtime",
+        "org.squeryl" % "squeryl_2.8.0.RC6" % "0.9.4beta6" % "compile->runtime",
         "org.mortbay.jetty" % "jetty" % "6.1.22" % "test->default",
         "com.h2database" % "h2" % "1.2.121",
         "cglib" % "cglib-nodep" % "2.2"
